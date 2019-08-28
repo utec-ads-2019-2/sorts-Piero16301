ef3104188a62a54560ced1e2ee54bf68810fe1c5
Piero Angelo <piero.morales@utec.edu.pe>
2019-08-28 18:53:02 -0500

    ....

---
+++ cmake-build-debug/CMakeFiles/Sorts.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sorts.dir/main.cpp.obj"
+  "Sorts.pdb"
+  "Sorts.exe"
+  "Sorts.exe.manifest"
+  "libSorts.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sorts.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
